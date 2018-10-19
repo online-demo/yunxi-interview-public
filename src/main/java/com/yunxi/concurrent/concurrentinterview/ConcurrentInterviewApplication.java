8c01e3e182cf908fbe415ff1f3bfadc2e6b3cf87
zhouguanya <zhouguanya@meituan.com>
2018-10-19 19:37:24 +0800

    update

---
+++ src/main/java/com/yunxi/concurrent/concurrentinterview/ConcurrentInterviewApplication.java
@@ -0,0 +1,12 @@
+package com.yunxi.concurrent.concurrentinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentInterviewApplication.class, args);
+    }
+}
